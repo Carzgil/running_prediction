17f745ddd524ed16d3d84a800831b85400fde45b
Aleksander Kapera <aleksanderkapera@gmail.com>
2024-11-29 16:17:26 -0500

    Add individual correlation analysis

---
+++ correlation_analysis/correlation_analysis.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def perform_correlation_analysis(data):
+    """
+    Perform Pearson correlation analysis focusing on correlations with injury column
+
+    Parameters:
+        data (pd.DataFrame): Input dataframe with daily data
+
+    Returns:
+        pd.Series: Correlations with injury column
+        pd.Series: P-values for correlations with injury
+    """
+    # Get correlations with injury column
+    # Calculate p-values for injury correlations
+    injury_correlations = []
+    p_values = []
+    for col in data.columns:
+        correlation, p_value = pearsonr(data[col], data['injury'])
+        injury_correlations.append(correlation)
+        p_values.append(p_value)
+
+    p_values_series = pd.Series(p_values, index=data.columns)
+    injury_correlations_series = pd.Series(
+        injury_correlations, index=data.columns)
+
+    # Plot correlation bar chart
+    # plt.figure(figsize=(12, 6))
+    # injury_correlations.drop('injury').sort_values().plot(kind='bar')
+    # plt.title('Feature Correlations with Injury')
+    # plt.xlabel('Features')
+    # plt.ylabel('Correlation Coefficient')
+    # plt.xticks(rotation=45, ha='right')
+    # plt.tight_layout()
+    # plt.show()
+
+    return injury_correlations_series, p_values_series
+
+
+if __name__ == "__main__":
+    # data = pd.read_csv('clustering/filtered_data_without_outliers.csv')
+    data = pd.read_csv('data/day_approach_maskedID_timeseries.csv')
+
+    # Filter columns from previous days
+    # keep_cols = data.columns[data.columns.str.endswith('.6') |
+    #                          data.columns.isin(['Athlete ID', 'injury', 'Date'])]
+    # data = data.loc[:, keep_cols]
+
+    # Group data by Athlete ID without aggregation
+    athlete_groups = {athlete_id: group for athlete_id,
+                      group in data.groupby('Athlete ID')}
+
+    # Filter for athletes with at least one injury
+    athlete_groups = {athlete_id: group for athlete_id, group in athlete_groups.items()
+                      if group['injury'].sum() > 0}
+    print(f"\nAnalyzing {len(athlete_groups)} athletes with injury history")
+
+    athletes_with_small_correlation = []
+    for athlete_id, group in athlete_groups.items():
+        print(f"Correlation analysis for athlete {athlete_id}:")
+
+        # Remove Date, and Athlete ID columns
+        group = group.drop(columns=['Date', 'Athlete ID'])
+
+        injury_correlations, p_values_series = perform_correlation_analysis(
+            group)
+
+        # Check if any correlation is less than 0.1
+        injury_correlations = injury_correlations.iloc[:-1]
+        if injury_correlations.abs().max() < 0.5:
+            athletes_with_small_correlation.append(athlete_id)
+
+    print(f"Athletes with small correlation: {
+          len(athletes_with_small_correlation)}")
+
+    # Remove athletes with small correlation
+    top_performing_data = data[~data['Athlete ID'].isin(
+        athletes_with_small_correlation)]
+
+    # Group data by Athlete ID without aggregation
+    athlete_groups = {athlete_id: group for athlete_id,
+                      group in top_performing_data.groupby('Athlete ID')}
+
+    for athlete_id, group in athlete_groups.items():
+        print(f"Correlation analysis for athlete {athlete_id}:")
+
+        group = group.drop(columns=['Date', 'Athlete ID'])
+
+        injury_correlations, p_values_series = perform_correlation_analysis(
+            group)
+
+        # filter out columns with correlation less than 0.5
+        injury_correlations = injury_correlations.drop('injury')
+        injury_correlations = injury_correlations[injury_correlations.abs() >= 0.5]
+
+        # Skip if there are no correlations
+        if len(injury_correlations) == 0:
+            continue
+
+        plt.figure(figsize=(12, 6))
+        injury_correlations.sort_values().plot(kind='bar')
+        plt.title(f'Feature Correlations with Injury (Athlete {athlete_id})')
+        plt.xlabel('Features')
+        plt.ylabel('Correlation Coefficient')
+        plt.xticks(rotation=45, ha='right')
+        plt.tight_layout()
+        plt.savefig(f'correlation_analysis/athlete_{athlete_id}.png')
+        plt.close()
